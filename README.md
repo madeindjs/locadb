df0e210e44b0f84edefc040f8df70eaa7d4f56ee
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-05-18 23:38:08 +0200

    Add routes

---
+++ README.md
@@ -0,0 +1,8 @@
+# LocaDB
+
+Locadex API using Typescript and MongoDB
+
+## Links
+
+- [Building RESTful Web APIs with Node.js, Express, MongoDB and TypeScript](https://itnext.io/building-restful-web-apis-with-node-js-express-mongodb-and-typescript-part-1-2-195bdaf129cf)
+
--- lib/app.ts
+++ lib/app.ts
@@ -1,15 +1,16 @@
-// lib/app.ts
-
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes/crmRoutes";
 
 class App {
 
     public app: express.Application;
+    public routePrv: Routes = new Routes();
 
     constructor() {
         this.app = express();
-        this.config();        
+        this.config();
+        this.routePrv.routes(this.app);
     }
 
     private config(): void{
