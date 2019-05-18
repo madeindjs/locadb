c0509d6fb09ccf28fa8f00cbd485ccb9c5120d8a
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-05-18 23:44:47 +0200

    Add routes for products

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -8,5 +8,28 @@ export class Routes {
                 message: 'GET request successfulll!!!!'
             })
         })
+
+        app.route('/products')
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'TODO: Get products'
+            })
+        })
+        app.route('/products/:id')
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'TODO: Get product'
+            })
+        })
+        .put((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'TODO: Update products'
+            })
+        })
+        .delete((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'TODO: Destroy products'
+            })
+        })
     }
 }
